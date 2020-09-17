d63303f845760ce68c070b120af839a0c3c302a0
Michal Czuper <mrczuper@gmail.com>
2020-09-18 02:06:02 +0300

    1.7-1.11 complete

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,11 +3,46 @@ import ReactDOM from 'react-dom'
 
 const Header = ({ text }) => <h1>{text}</h1>
 
-const Display = ({ category, num }) => <p>{category} {num}</p>
+const Statistic = ({ category, num, end }) => {
+  return (
+    <tr>
+      <td>{category}</td><td>{num}{end}</td>
+    </tr>
+  )
+}
 
-const Button = ({handleClick, text}) => {
+const Button = ({ handleClick, text }) => {
   return (
     <button onClick={handleClick}>{text}</button>
+  )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+
+  let all = good + neutral + bad
+
+  if (all !== 0) {
+
+    let average_num = ((good - bad) / all).toFixed(1)
+    let positive_num = Math.round((good / all) * 100).toFixed(1)
+
+    return (
+      <table>
+        <tbody>
+          <Statistic category={"good"} num={good} />
+          <Statistic category={"neutral"} num={neutral} />
+          <Statistic category={"bad"} num={bad} />
+          <Statistic category={"all"} num={good + neutral + bad} />
+          <Statistic category={"average"} num={average_num} />
+          <Statistic category={"positive"} num={positive_num} end={"%"} />
+        </tbody>
+      </table>
+    )
+  }
+  return (
+    <div>
+      <p>No feedback given</p>
+    </div>
   )
 }
 
@@ -19,28 +54,23 @@ const App = () => {
 
   const give_feedback = "give feedback"
   const statistics = "statistics"
-  const good_text = "good"
-  const neutral_text = "neutral"
-  const bad_text = "bad"
 
   return (
     <div>
       <Header text={give_feedback} />
-      
-      <Button handleClick={() => setGood(good + 1)} text={"good"}/>
-      <Button handleClick={() => setNeutral(neutral + 1)} text={"neutral"}/>
+
+      <Button handleClick={() => setGood(good + 1)} text={"good"} />
+      <Button handleClick={() => setNeutral(neutral + 1)} text={"neutral"} />
       <Button handleClick={() => setBad(bad + 1)} text={"bad"} />
 
       <Header text={statistics} />
-      
-      <Display category={good_text} num={good}/>
-      <Display category={neutral_text} num={neutral}/>
-      <Display category={bad_text} num={bad}/>
+
+      <Statistics good={good} neutral={neutral} bad={bad} />
 
     </div>
   )
 }
 
-ReactDOM.render(<App />, 
+ReactDOM.render(<App />,
   document.getElementById('root')
 )
