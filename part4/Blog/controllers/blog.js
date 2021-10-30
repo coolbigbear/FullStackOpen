55da0930f73563ff7e9691c20e4ee8ced35c43b8
Michal Czuper <mrczuper@gmail.com>
2021-10-30 06:48:42 +0300

    Done 4.2

---
+++ part4/Blog/controllers/blog.js
@@ -0,0 +1,22 @@
+const BlogRouter = require('express').Router()
+const Blog = require('../models/Blog')
+
+BlogRouter.get('/', (request, response) => {
+    Blog
+        .find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
+})
+
+BlogRouter.post('/', (request, response) => {
+    const blog = new Blog(request.body)
+
+    blog
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
+})
+
+module.exports = BlogRouter
